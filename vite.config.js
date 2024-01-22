a9ccc7c8e5e55517ad139c08d93bd39c99d4b78b
JaccobStanton <jstanton23@my.stlcc.edu>
2024-01-22 12:01:31 -0600

    Removed unneeded import

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import svgLoader from "vite-svg-loader";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
