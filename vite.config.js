2e191a4f8d14c58f8ae262aea6b12247e8da7990
JaccobStanton <jstanton23@my.stlcc.edu>
2024-01-22 11:45:00 -0600

    Installed npm install --save-dev vite-plugin-svgr to allow SVG's to be imported as components

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import svgLoader from "vite-svg-loader";
+// import svgLoader from "vite-svg-loader";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgLoader()],
+  plugins: [react(), svgr()],
 });
