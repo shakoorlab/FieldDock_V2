ed1c04cf4a5f942249bbcc9a65dde81867f17d5d
JaccobStanton <jstanton23@my.stlcc.edu>
2024-01-22 19:22:04 -0600

    Added 3 routes, HomePage, RealtimeSettings, and Download

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Layout from "./components/Layout/Layout";
+import HomePage from "./scenes/dashboard/HomePage";
+import Download from "./scenes/dashboard/Download";
+import RealtimeSettings from "./scenes/dashboard/RealtimeSettings";
 
 const App = () => {
   return (
     <Router>
       <Layout>
         <Routes>
-          {/* <Route path="/" element={<HomePage />} /> */}
-          {/* <Route path="/about" element={<AboutPage />} /> */}
-          {/* Add more routes as needed */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Realtime-Settings" element={<RealtimeSettings />} />
+          <Route path="/Download" element={<Download />} />
         </Routes>
       </Layout>
     </Router>
