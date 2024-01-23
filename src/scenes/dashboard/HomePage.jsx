27c7a01f3299d4352b99df980445088907d1f8e0
JaccobStanton <jstanton23@my.stlcc.edu>
2024-01-22 19:27:50 -0600

    Created HomePage component, for when user opens the application

---
+++ src/scenes/dashboard/HomePage.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "../../index.css";
+import GDD from "../../components/Index/GDD";
+import SystemStatus from "../../components/Index/SystemStatus";
+import Rainfall from "../../components/Index/Rainfall";
+import Misc from "../../components/Index/Misc";
+import EnvConds from "../../components/Index/EnvCond";
+import SoilConds from "../../components/Index/SoilConditions";
+
+function HomePage() {
+  return (
+    <>
+      <div className="main-grid">
+        <GDD />
+        <SystemStatus />
+        <Rainfall />
+        <Misc />
+        <div className="second-row-child-container">
+          <div className="svg-container">
+            <img
+              src="src/assets/svg/Drone_FieldDock.svg"
+              className="drone-svg"
+              alt="FieldDock drone diagram"
+            />
+          </div>
+        </div>
+        <EnvConds />
+        <SoilConds />
+        <div className="second-row-child-container">
+          <div className="svg-container">
+            <img
+              src="src/assets/svg/FieldDock_Body.svg"
+              className="body-svg"
+              alt="FieldDock body"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
