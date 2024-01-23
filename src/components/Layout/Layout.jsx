f965804c1193c5fc35f42e3dcda2d6526fb5a796
JaccobStanton <jstanton23@my.stlcc.edu>
2024-01-22 19:24:58 -0600

    Made the layout page the default landing page, with Navbar attached, and children to select when navbar navigates

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,9 +2,8 @@ import React from "react";
 
 import { Container } from "react-bootstrap";
 import Navbar from "../NavBar/NavBar";
-import Index from "../../scenes/dashboard/Index";
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <div
       style={{
@@ -21,13 +20,22 @@ const Layout = () => {
           width: "92%",
           borderRadius: "8px",
           border: "1px solid #012a44",
+          height: "120vh",
+          backgroundColor: "rgba(0, 0, 0, 0)",
+          backgroundImage: `
+          radial-gradient(
+            circle at 50% 0,
+            rgba(68, 68, 68, 0.6),
+            #020202 79%,
+            rgba(0, 40, 53, 0.66)
+          ),
+          linear-gradient(rgba(1, 0, 15, 0.89), rgba(1, 0, 15, 0.89))
+        `,
         }}
       >
         <div className="gradient-overlay">
-          <div className="main-grid">
-            <Navbar />
-            <Index />
-          </div>
+          <Navbar />
+          <main>{children}</main>
         </div>
       </Container>
     </div>
