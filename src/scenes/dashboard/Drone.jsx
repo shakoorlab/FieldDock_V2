9945241ed3e3a2d9d545a9c3286416baeadfb39d
JaccobStanton <jstanton23@my.stlcc.edu>
2024-02-05 19:19:08 -0600

    Created 'Drone' page component

---
+++ src/scenes/dashboard/Drone.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Drone() {
+  return (
+    <>
+      <div className="page-title-box">
+        <h3>Drone</h3>
+      </div>
+    </>
+  );
+}
+export default Drone;
