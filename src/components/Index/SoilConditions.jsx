545d70c0fb31bbfa8385c385ad1c3572d7753fca
JaccobStanton <jstanton23@my.stlcc.edu>
2024-02-08 14:29:47 -0600

    Added <SoilSensorTable /> to the SoilConds component

--- src/components/Index/SoilConditions.jsx
+++ src/components/Index/SoilConditions.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import SoilSensorTable from "./SoilSensorTable";
 
 function SoilConds() {
   return (
     <>
       <div className="row-parent-box">
         <div className="title-box">Soil Conditions</div>
+        <div className="misc-soil-table-parent">
+          <div className="menu-parent">
+            <select className="table-select-menu">
+              <option>Select a sensor...</option>
+            </select>
+          </div>
+          {/* New parent element to appear underneath the select menu */}
+          <div className="content-parent">
+            <SoilSensorTable />
+          </div>
+        </div>
       </div>
     </>
   );
