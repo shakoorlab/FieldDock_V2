7a9dd26f33bc573569a7ddf35f3198a74c0ceeed
JaccobStanton <jstanton23@my.stlcc.edu>
2024-02-08 14:27:27 -0600

    Created <Imaging /> component

---
+++ src/scenes/dashboard/Imaging.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Imaging() {
+  return (
+    <>
+      <div className="page-title-box">
+        <h3>Imaging</h3>
+      </div>
+      <div className="button-container">
+        <button className="choice-button">FieldDock Imager</button>
+        <button className="choice-button">Mosaicked Images</button>
+      </div>
+    </>
+  );
+}
+
+export default Imaging;
