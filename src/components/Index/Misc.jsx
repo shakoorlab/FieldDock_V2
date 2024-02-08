b2449c9f7c16702390ee32a0b5999c6726fb5bd9
JaccobStanton <jstanton23@my.stlcc.edu>
2024-02-08 14:28:24 -0600

    Added <MiscTable /> component to the Misc component

--- src/components/Index/Misc.jsx
+++ src/components/Index/Misc.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import MiscSensorTable from "./MiscTable";
 
 function Misc() {
   return (
     <>
       <div className="row-parent-box">
         <div className="title-box">Misc</div>
-        <div className="table-parent">
+        <div className="misc-soil-table-parent">
           <div className="menu-parent">
             <select className="table-select-menu">
               <option>Select a sensor...</option>
             </select>
+          </div>
+          {/* New parent element to appear underneath the select menu */}
+          <div className="content-parent">
+            <MiscSensorTable />
           </div>
         </div>
       </div>
