dad59e8a09d2619276b5618834dd5599b06426e4
JaccobStanton <jstanton23@my.stlcc.edu>
2024-02-08 20:42:07 -0600

    Began work on <PlannedMissionsParent /> component

--- src/components/Drone/LandingPage/PlannedMissionsParent.jsx
+++ src/components/Drone/LandingPage/PlannedMissionsParent.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import NoMissionsPlanned from "./NoMissions";
+import NoMissionsPlanned from "../Missions/NoMissions";
 
 function PlannedMissionsParent() {
   return (
     <>
       <div className="row-parent-box environmental-conditions-box">
         <div className="title-box">Planned Missions</div>
+        {/* <div
+          style={{
+            height: "500px",
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+          }}
+        >
+          <div style={{ height: "80%", backgroundColor: "#f0f0f0" }}>
+          </div>
+          <div style={{ height: "20%", backgroundColor: "#d0d0d0" }}>
+           
+          </div>
+        </div> */}
         <NoMissionsPlanned />
       </div>
     </>
