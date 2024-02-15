1594fc765879e9df799438a2b37565aebafebd93
JaccobStanton <jstanton23@my.stlcc.edu>
2024-02-14 23:13:52 -0600

    Added modal, and DateTime components from MUI

--- src/components/Drone/Dashboard/CreateMission/CreateMissionDrawer.jsx
+++ src/components/Drone/Dashboard/CreateMission/CreateMissionDrawer.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useCallback } from "react";
-// import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-// import axios from 'axios';
-import { Box, Grid, Typography, Paper } from "@mui/material";
+import { Box, Grid, Typography, Paper, Modal } from "@mui/material";
 import LatLongTable from "../CreateMission/LatLongTable";
 import Map from "../CreateMission/MapComponent"; // Assuming this is your map component
 import { styled } from "@mui/material/styles";
+import { LocalizationProvider } from "@mui/x-date-pickers-pro";
+import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import Grow from "@mui/material/Grow";
 
 // Styled component for reusability
 const Item = styled(Paper)(({ theme }) => ({
@@ -15,7 +18,28 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  borderRadius: "4px",
+  transform: "translate(-50%, -50%)",
+  width: "50%",
+  height: "52%",
+  border: "2.5px solid #797979",
+  boxShadow: "0 7px 5px 1px rgba(0, 0, 0, 0.2)",
+  p: 4,
+  background: "linear-gradient(1deg, rgba(0, 0, 0, 0.95), rgb(27, 27, 27))",
+  display: "flex",
+  flexDirection: "row",
+};
+
 const CreateMissionDrawer = () => {
+  const [open, setOpen] = useState(false); // Modal is initially closed
+
+  const handleOpen = () => setOpen(true); // Function to open the modal
+  const handleClose = () => setOpen(false); // Function to close the modal
+
   const [data, setData] = useState([
     {
       id: 1,
@@ -249,7 +273,81 @@ const CreateMissionDrawer = () => {
 
                   <button className="create-mission-buttons">Load</button>
 
-                  <button className="create-mission-buttons-plan">Plan</button>
+                  <button
+                    onClick={handleOpen}
+                    className="create-mission-buttons-plan"
+                  >
+                    Plan
+                  </button>
+                  <Modal open={open} onClose={handleClose}>
+                    <Box sx={modalStyle}>
+                      {/* Content inside your modal */}
+
+                      <Box
+                        sx={{
+                          display: "flex",
+                          width: "50%",
+                          height: "100%",
+                          bgcolor: "transparent",
+                          borderRight: "1px solid #797979", // Adds a border between the boxes
+                        }}
+                      >
+                        {/* Content of the first box */}
+                      </Box>
+                      <Grow
+                        in={true}
+                        style={{ transformOrigin: "0 0 0" }}
+                        timeout={1100}
+                      >
+                        <Box
+                          sx={{
+                            width: "50%",
+                            height: "100%",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                        >
+                          <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DateCalendar
+                              showDaysOutsideCurrentMonth
+                              views={["day", "month"]}
+                              sx={{
+                                // width: "90% !important",
+                                borderRadius: "4px",
+                                border: "1px solid #797979",
+                                boxShadow: "0 7px 5px 1px rgba(0, 0, 0, 0.2)",
+                                background:
+                                  "linear-gradient(1deg, rgba(0, 0, 0, 0), #1b1b1b)",
+                                marginBottom: "20px",
+                              }}
+                            />
+                            <Box
+                              sx={{
+                                display: "flex",
+                                width: "100%", // Ensure the wrapper fills the parent box
+                                justifyContent: "center", // Center the TimePicker horizontally
+                              }}
+                            >
+                              <TimePicker
+                                label="Select a time.."
+                                name="Select a time.."
+                                sx={{
+                                  width: "83.5% !important",
+                                  borderRadius: "4px",
+                                  border: "1px solid #797979",
+                                  boxShadow: "0 7px 5px 1px rgba(0, 0, 0, 0.2)",
+                                  background:
+                                    "linear-gradient(1deg, rgba(0, 0, 0, 0), #1b1b1b)",
+                                }}
+                              />
+                            </Box>
+                          </LocalizationProvider>
+                        </Box>
+                      </Grow>
+                    </Box>
+                  </Modal>
                 </div>
               </div>
             </Item>
@@ -268,4 +366,29 @@ export default CreateMissionDrawer;
                   style={{ transformOrigin: "0 0 0" }}
                   timeout={1100}
                 ></Grow> */
+}
+
+{
+  /* <LocalizationProvider dateAdapter={AdapterDayjs}>
+                          <div className="customCalendar">
+                            <Grow
+                              in={true}
+                              style={{ transformOrigin: "0 0 0" }}
+                              timeout={1000}
+                            >
+                              <DateCalendar
+                                showDaysOutsideCurrentMonth
+                                views={["day", "month"]}
+                                sx={{
+                                  // width: "90% !important",
+                                  borderRadius: "4px",
+                                  border: "1px solid #797979",
+                                  boxShadow: "0 7px 5px 1px rgba(0, 0, 0, 0.2)",
+                                  background:
+                                    "linear-gradient(1deg, rgba(0, 0, 0, 0), #1b1b1b)",
+                                }}
+                              />
+                            </Grow>
+                          </div>
+                        </LocalizationProvider> */
 }
