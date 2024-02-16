17199ee689eb9c5c5b6914b0b27d0cf5d0b3437d
JaccobStanton <jstanton23@my.stlcc.edu>
2024-02-15 23:22:02 -0600

    Created a long column version of the 'NoMissions' for the drone dashboard mission columns

---
+++ src/components/Drone/Dashboard/NoMissions.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NoMissionsDisplayed = () => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "1.5rem",
+        color: "#797979",
+        opacity: "30%",
+      }}
+    >
+      No missions to display
+    </div>
+  );
+};
+
+export default NoMissionsDisplayed;
