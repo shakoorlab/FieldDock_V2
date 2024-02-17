301aea332bc9cea709df101627c047f87f612a70
JaccobStanton <jstanton23@my.stlcc.edu>
2024-02-16 18:57:21 -0600

    Removed unneeded route

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,6 @@ import Diagnostics from "./scenes/dashboard/Diagnostics";
 import Users from "./scenes/dashboard/Users";
 import Download from "./scenes/dashboard/Download";
 import DroneDashboard from "./components/Drone/Dashboard/DroneDash";
-import CreateMissionDrawer from "./components/Drone/Dashboard/CreateMission/CreateMissionDrawer";
 
 const LayoutWithNavbar = () => (
   <Layout>
@@ -55,10 +54,10 @@ const App = () => {
 
           <Route path="/drone-dashboard" element={<DroneDashboard />} />
 
-          <Route
+          {/* <Route
             path="/create-mission-drawer"
             element={<CreateMissionDrawer />}
-          />
+          /> */}
         </Routes>
       </LoadScript>
     </Router>
