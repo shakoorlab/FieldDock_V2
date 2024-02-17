5f9cb29376602da0897cd89d2674816453c299ff
JaccobStanton <jstanton23@my.stlcc.edu>
2024-02-16 19:02:39 -0600

    Updated button logic to be click on an render another component

--- src/scenes/dashboard/Imaging.jsx
+++ src/scenes/dashboard/Imaging.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import FieldDockImager from "../../components/Imaging/FieldDockImager";
+import AnalyzedImages from "../../components/Imaging/AnalyzedImages";
 
 function Imaging() {
+  const [selectedOption, setSelectedOption] = useState(null);
   return (
     <>
       <div className="page-title-box">
         <h3>Imaging</h3>
       </div>
       <div className="button-container">
-        <button className="choice-button">FieldDock Imager</button>
-        <button className="choice-button">Mosaicked Images</button>
+        {/* Add onClick handlers to set the selected option */}
+        <button
+          className="choice-button"
+          onClick={() => setSelectedOption("FieldDock")}
+        >
+          FieldDock Imager
+        </button>
+        <button
+          className="choice-button"
+          onClick={() => setSelectedOption("Analyzed")}
+        >
+          Analyzed Images
+        </button>
+      </div>
+      {/* Conditionally render the appropriate component based on the selected option */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "4rem",
+        }}
+      >
+        {selectedOption === "FieldDock" && <FieldDockImager />}
+        {selectedOption === "Analyzed" && <AnalyzedImages />}
       </div>
     </>
   );
